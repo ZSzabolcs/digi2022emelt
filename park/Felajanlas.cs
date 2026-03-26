20923cf3fe8bfc650fcdc68f3f2822d275ede411
ZSzabolcs <zelenaksz@kkszki.hu>
2026-03-26 09:39:27 +0100

    5. feladat mentés

--- park/Felajanlas.cs
+++ park/Felajanlas.cs
@@ -8,20 +8,17 @@ namespace park
 {
     public class Felajanlas
     {
-        private string? db;
         private int? tol;
         private int? ig;
         private char? szin;
 
-        public Felajanlas(string? db, int? tol, int? ig, char? szin)
+        public Felajanlas(int? tol, int? ig, char? szin)
         {
-            this.Db = db;
             this.Tol = tol;
             this.Ig = ig;
             this.Szin = szin;
         }
 
-        public string? Db { get => db; set => db = value; }
         public int? Tol { get => tol; set => tol = value; }
         public int? Ig { get => ig; set => ig = value; }
         public char? Szin { get => szin; set => szin = value; }
--- park/Program.cs
+++ park/Program.cs
@@ -10,10 +10,10 @@
             while (!sr.EndOfStream)
             {
                 var sor = sr.ReadLine().Split(' ');
-                felajanlasok.Add(new Felajanlas(db, Convert.ToInt32(sor[0]), Convert.ToInt32(sor[1]), Convert.ToChar(sor[2])));
+                felajanlasok.Add(new Felajanlas(Convert.ToInt32(sor[0]), Convert.ToInt32(sor[1]), Convert.ToChar(sor[2])));
             }
             sr.Close();
-            Console.WriteLine($"2. feladat\nA felajánlások száma: {db}\n");
+            Console.WriteLine($"2. feladat\nA felajánlások száma: {felajanlasok.Count()}\n");
             Console.WriteLine("3. feladat\nA bejárat bal és jobb oldalát is beültető(k) sorszáma(i):");
             for (int i = 0; i < felajanlasok.Count; i++)
             {
@@ -49,7 +49,7 @@
                     szinek.Add(Convert.ToChar(item.Szin));
                 }
             }
-            if(szinek.Count > 0)
+            if (szinek.Count > 0)
             {
                 Console.Write($"A virágágyás színei:");
                 foreach (var item in szinek)
@@ -57,6 +57,11 @@
                     Console.Write($" {item}");
                 }
             }
+            Console.WriteLine($"5. feladat");
+            string lehetoseg1 = "Minden ágyás beültetésére van jelentkező.";
+            string lehetoseg2 = "Átszervezéssel megoldható a beültetés.";
+            string lehetoseg3 = "A beültetés nem oldható meg.";
+            
         }
     }
 }
